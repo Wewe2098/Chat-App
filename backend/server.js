e2dd2395b0e934d7e2ea0983860074dc3d3da3ad
Sphiwe Mkwatheni <43848182+Wewe2098@users.noreply.github.com>
2024-04-25 22:19:15 +0200

    latest changes

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const cookieParser = require('cookie-parser');
+const path = require('path'); // Import the 'path' module
 const authRoutes = require('./routes/auth.routes.js'); 
 const messageRoutes = require('./routes/message.routes.js'); 
 const userRoutes = require('./routes/user.routes.js'); 
 const connectToMongoDB = require('./db/connectToMongoDB.js');
 const { app, server } = require('./socket/socket.js'); 
 
-dotenv.config();
-
 const PORT = process.env.PORT || 5000;
+
+dotenv.config();
 
 app.use(express.json());
 app.use(cookieParser());
@@ -18,11 +19,15 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-// app.get("/", (req, res) => {
-//  res.send("Hello World!!");
-//});
+// Serve static files from the frontend build directory
+app.use(express.static(path.join(__dirname, "frontend", "dist")));
+
+// Route all other requests to the frontend index.html
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(PORT, () => {
-  connectToMongoDB();
-  console.log(`Server Running on port ${PORT}`);
+	connectToMongoDB();
+	console.log(`Server Running on port ${PORT}`);
 });
