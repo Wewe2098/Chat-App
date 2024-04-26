28857588e2fc7ac553adeb1f6ebf5edfe49ae823
Sphiwe Mkwatheni <43848182+Wewe2098@users.noreply.github.com>
2024-04-26 15:25:49 +0200

    new change

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,18 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const cookieParser = require('cookie-parser');
-const authRoutes = require('./routes/auth.routes.js'); 
-const messageRoutes = require('./routes/message.routes.js'); 
-const userRoutes = require('./routes/user.routes.js'); 
-const connectToMongoDB = require('./db/connectToMongoDB.js'); 
-const { app, server } = require('./socket/socket.js'); 
+import path from "path";
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
+import connectToMongoDB from "./db/connectToMongoDB.js";
+import { app, server } from "./socket/socket.js";
 
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
 
 dotenv.config();
 
@@ -19,6 +23,11 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(PORT, () => {
 	connectToMongoDB();
