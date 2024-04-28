ad5d8ea7d1526d7e376cd12f5bc8ffe5b5c96c59
Sphiwe Mkwatheni <43848182+Wewe2098@users.noreply.github.com>
2024-04-28 17:07:16 +0200

    changes

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,6 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import axios from 'axios';
 
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
@@ -15,45 +14,25 @@ import { app, server } from "./socket/socket.js";
 dotenv.config();
 
 const __dirname = path.resolve();
+// PORT should be assigned after calling dotenv.config() because we need to access the env variables. Didn't realize while recording the video. Sorry for the confusion.
 const PORT = process.env.PORT || 5000;
 
-app.use(express.json());
+app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
 app.use(cookieParser());
+
 app.use(cors());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-
-// Set up Axios instance with default headers
-const axiosInstance = axios.create({
-  baseURL: 'https://chat-app-phxe.onrender.com/api',
-  headers: {
-    'Authorization': `Bearer ${authToken}`,
-    'Content-Type': 'application/json',
-  },
-});
-
-// Route handler for fetching users with authentication
-app.get("/api/fetch-users", async (req, res) => {
-  try {
-    // Make a request to the /api/users endpoint with authentication
-    const response = await axiosInstance.get('/users');
-    res.json(response.data);
-  } catch (error) {
-    console.error('Error fetching users:', error);
-    res.status(500).json({ error: 'Failed to fetch users' });
-  }
-});
-
 app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
 
 server.listen(PORT, () => {
-  connectToMongoDB();
-  console.log(`Server Running on port ${PORT}`);
+	connectToMongoDB();
+	console.log(`Server Running on port ${PORT}`);
 });
